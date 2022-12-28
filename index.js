5a0c03d30289f2ae426d3f431e9eea4662aab979
이효원 <ymj07168@gmail.com>
2022-12-28 23:41:05 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ymj07168:abcd1234@boilerplate.7wa1qnm.mongodb.net/?retryWrites=true&w=majority', {
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+mongoose.set('strictQuery', false)
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
