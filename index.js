c19685a9c6162a3b339207f48595321a4008afab
이효원 <ymj07168@gmail.com>
2022-12-31 17:26:00 +0900

    BodyParser&PostMan&회원가입기능

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ymj07168:abcd1234@boilerplate.7wa1qnm.mongodb.net/?retryWrites=true&w=majority', {
@@ -10,5 +17,18 @@ mongoose.connect('mongodb+srv://ymj07168:abcd1234@boilerplate.7wa1qnm.mongodb.ne
 mongoose.set('strictQuery', false)
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
