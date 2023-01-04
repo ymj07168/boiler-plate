94fe2dc92c96e90bc8ef6400b0f27a618d33fd70
이효원 <ymj07168@gmail.com>
2023-01-05 00:33:24 +0900

    bcrypt로 비밀번호 암호화하기, 로그인 기능

--- index.js
+++ index.js
@@ -32,6 +32,28 @@ app.post('/register', (req, res) => {
             success: true
         })
     })
+
+    app.post('/login', (req, res) => {
+        //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+        User.findOne({ email: req.body.email }, (err, userInfo) => {
+            if (!userInfo) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "제공된 이메일에 해당하는 유저가 없습니다."
+                })
+            }
+        })
+        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+    
+            //비밀번호까지 맞다면 토큰을 생성한다.
+            user.generateToken((err, user) => {
+                
+            })
+        })
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
