141b4f1cf199df2f3b778aa576ca061e3ff4635a
이효원 <ymj07168@gmail.com>
2023-01-07 22:51:52 +0900

    참고 #2, auth 기능 만들기

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 
@@ -22,7 +22,7 @@ mongoose.set('strictQuery', false)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
 
     const user = new User(req.body)
@@ -35,7 +35,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -44,6 +44,7 @@ app.post('/login', (req, res) => {
                 message: "제공된 이메일에 해당하는 유저가 없습니다."
             })
         }
+
         //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
         user.comparePassword(req.body.password, (err, isMatch) => {
             if (!isMatch)
@@ -58,6 +59,22 @@ app.post('/login', (req, res) => {
                     .json({ loginSuccess: true, userId: user._id })
             })
         })
+    })
+})
+
+
+app.get('/api/users/auth', auth, (req, res) => {
+    //여기까지 미들웨어를 통과해왔다는 얘기는 authentication이 true라는 것
+
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        latname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
     })
 })
 
--- models/User.js
+++ models/User.js
@@ -25,6 +25,7 @@ const userSchema = mongoose.Schema({
         type: Number,
         default: 0
     },
+    image: String,
     token: {
         type: String
     },
@@ -73,6 +74,20 @@ userSchema.methods.generateToken = function (cb) {
     user.save(function (err, user) {
         if (err) return cb(err)
         cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function (err, decode) {
+        // 유저 id를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 token과 db에 보관된 토큰과 일치하는지 확인
+
+        user.findOne({ "_id": decode, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        })
     })
 }
 
