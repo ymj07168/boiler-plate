54b15058217fb2293df36b193bfbcc58664a681f
이효원 <ymj07168@gmail.com>
2023-01-13 17:20:49 +0900

    feat: 비디오 저장

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.use(cookieParser());
 
 
 app.use('/api/users', require('./routes/users'));
-
+app.use('api/video', require('./routes/video'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/video.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+// const { Video } = require("../models/Video");
+
+const { auth } = require("../middleware/auth");
+const multer = require('multer');
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filenmae: (req, file, cb) => {
+        cb(null, `${Data.now()}_${file.originalname}`);
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extnmae(file.orginalname)
+        if (ext !== '.mp4') {
+            return cb(res.status(400).end('only mp4 is allowed'), false);
+        }
+        cb(null, true)
+    }
+});
+
+
+const upload = multer({ storage: storage }).single("file");
+
+//=================================
+//             Video
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+    // 비디오를 서버에 저장한다.
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err})
+        }
+        return res.json({ success: true, url: res.req.file.path, filename: res.req.file.filename })
+    })
+})
+
+module.exports = router;
