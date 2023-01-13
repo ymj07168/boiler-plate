637b8c56df0811f4e4ab4759acb20091139ddae8
이효원 <ymj07168@gmail.com>
2023-01-13 17:51:00 +0900

    feat: 비디오 정보 저장

---
+++ models/Video.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const videoSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    privacy: {
+        type: Number
+    },
+    category: {
+        type: String
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    duration: {
+        type: String
+    },
+    thumbnail: {
+        type: String
+    }
+}, { timestamps: true })
+
+const Video = mongoose.model('Video', videoSchema)
+
+module.exports = { Video }
